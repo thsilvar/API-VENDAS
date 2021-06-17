cc5174f832fe2bc66250927094291f354744ba7b
Thiago Silva <thsilvar@gmail.com>
2021-06-17 15:10:20 -0300

    Configuracoes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,36 @@
-console.log('Hello Dev!')
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import routes from './routes'
+import AppError from '@shared/errors/AppError';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.use(
+    (
+        error: Error,
+        request: Request,
+        response:Response,
+        next: NextFunction,
+    )=>{
+        if(error instanceof AppError){
+            return response.status(error.statusCode).json({
+                status:'erro',
+                message: error.message,
+            });
+        }
+        return response.status(500).json({
+            status:'erros',
+            message:'Internal server error',
+        })
+        },
+);
+
+
+app.listen(3333, ()=>{
+    console.log('Server Startd on port 3333!')
+})
