870132c1c9de7797240bc008da8a2b1a0f9f754b
Thiago Silva <thsilvar@gmail.com>
2021-06-18 09:54:11 -0300

    Create ListUserService.ts
    Criação serviço Listar usuarios

---
+++ src/modules/users/services/ListUserService.ts
@@ -0,0 +1,18 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+
+class ListUserService {
+  public async execute(): Promise<User[]> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+
+    const user = usersRepository.find();
+
+
+    return user;
+  }
+}
+
+export default ListUserService;
