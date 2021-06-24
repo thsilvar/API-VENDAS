1cc1eb58637ddc2c567e9ec44df6651339d59f68
Thiago Silva <thsilvar@gmail.com>
2021-06-24 19:44:10 -0300

    Criando a rota de Customers

--- src/modules/customers/controllers/CustomersController.ts
+++ src/modules/customers/controllers/CustomersController.ts
@@ -5,60 +5,60 @@ import ListCustomerService from "../services/ListCustomerService";
 import ShowCustomerService from "../services/ShowCustomerService";
 import UpdateCustomerService from "../services/UpdateCustomerService";
 
-export default class CustomersController{
-    public async index(requst:Request, responde: Response): Promise<Response>{
-        const listCustomers = new ListCustomerService();
-
-        const customers = await listCustomers.execute();
-
-        return responde.json(customers);
+export default class CustomersController {
+    public async index(request: Request, response: Response): Promise<Response> {
+      const listCustomers = new ListCustomerService();
+ 
+      const customers = await listCustomers.execute();
+ 
+      return response.json(customers);
     }
-
-    public async show(request:Request, response:Response): Promise<Response>{
-        const {id} = request.params;
-
-        const showCustomer = new ShowCustomerService();
-
-        const customers = await showCustomer.execute({id});
-
-        return response.json(customers);
+ 
+    public async show(request: Request, response: Response): Promise<Response> {
+      const { id } = request.params;
+ 
+      const showCustomer = new ShowCustomerService();
+ 
+      const customer = await showCustomer.execute({ id });
+ 
+      return response.json(customer);
     }
-
-    public  async create(request:Request, response:Response): Promise<Response>{
-        const {name, email} = request.body;
-
-        const createCustomer = new CreateCustomerService();
-
-        const customer = await createCustomer.execute({
-            name, 
-            email
-        })
-
-        return response.json(customer);
+ 
+    public async create(request: Request, response: Response): Promise<Response> {
+      const { name, email } = request.body;
+ 
+      const createCustomer = new CreateCustomerService();
+ 
+      const customer = await createCustomer.execute({
+        name,
+        email,
+      });
+ 
+      return response.json(customer);
     }
-
-    public  async update(request:Request, response:Response): Promise<Response>{
-        const {name, email} = request.body;
-        const {id} = request.params;
-
-        const updateCustomer = new UpdateCustomerService();
-
-        const customer = await updateCustomer.execute({
-            id,
-            name, 
-            email
-        })
-
-        return response.json(customer);
+ 
+    public async update(request: Request, response: Response): Promise<Response> {
+      const { name, email } = request.body;
+      const { id } = request.params;
+ 
+      const updateCustomer = new UpdateCustomerService();
+ 
+      const customer = await updateCustomer.execute({
+        id,
+        name,
+        email,
+      });
+ 
+      return response.json(customer);
     }
-
-    public async delete(request:Request, response:Response): Promise<Response>{
-        const {id} = request.params;
-
-        const deleteCustomer = new DeleteCustomerService();
-
-       await deleteCustomer.execute({id});
-
-        return response.json([]);
+ 
+    public async delete(request: Request, response: Response): Promise<Response> {
+      const { id } = request.params;
+ 
+      const deleteCustomer = new DeleteCustomerService();
+ 
+      await deleteCustomer.execute({ id });
+ 
+      return response.json([]);
     }
-}
+  }
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import customersRouter from "@modules/customers/routes/customers.routes";
 import productsRouter from "@modules/products/routes/products.routes";
 import passwordRouter from "@modules/users/routes/password.routes";
 import profileRouter from "@modules/users/routes/profile.routes";
@@ -12,5 +13,6 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRouter);
 routes.use('/profile', profileRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
