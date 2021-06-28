30a6eed9fa38a27dc9404e2c6e972ed437228446
Thiago Silva <thsilvar@gmail.com>
2021-06-28 16:41:54 -0300

    cache install

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,16 +1,22 @@
 import { getCustomRepository } from "typeorm";
 import Product from "../typeorm/entities/Product";
 import ProductRepository from "../typeorm/repositories/ProductsRepository";
-
+import RedisCache from "@shared/cache/RedisCache";
 
 
 class ListProductService {
   public async execute(): Promise<Product[]> {
     const productsRepository = getCustomRepository(ProductRepository);
 
+    const redisCache = new RedisCache();
 
-    const products = productsRepository.find();
+    let products = await redisCache.recover<Product[]>('api-vendas-PRODUCT_LIST');
 
+    if(!products){
+      products =  await productsRepository.find();
+
+      await redisCache.save('api-vendas-PRODUCT_LIST', products);
+    }
 
     return products;
   }
